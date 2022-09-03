bd29dccb6f6e28476bcb7257d6f33572d3ac8463
Bibek979 <bibekmeananda@gmail.com>
2022-09-03 12:07:32 +0530

    The navbar changes with login

--- src/Components/ComponentsNavbar/Navbar.js
+++ src/Components/ComponentsNavbar/Navbar.js
@@ -3,11 +3,17 @@ import { Link } from 'react-router-dom';
 import './navbar__style.css';
 import brandImg from './brandLogo.png';
 import { useRef } from 'react';
-
+import { AdminLoggedIn } from '../../Helper/Context';
+import { IsLoggedIn } from '../../Helper/Context';
+import { useContext } from 'react';
 // Thanks for the logo - <a target="_blank" href="https://icons8.com/icon/SVUqR31B1Sl9/stock">Stock</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
 
 function Navbar(){
     const myRef = useRef(null);
+    const userLogin = useContext(IsLoggedIn);
+    const adminLogin = useContext(AdminLoggedIn);
+    console.log(adminLogin.adminLogin);
+    console.log(userLogin.userLogin);
     const executeScroll = () =>
         myRef.current.scrollIntoView();
     return(
@@ -21,7 +27,12 @@ function Navbar(){
                     <Link to={myRef} onClick={executeScroll}>About</Link>
                     <Link to='/learn'>Learn</Link>
                     <Link to='/course'>Courses</Link>
-                    <Link to='/signup'>Sign Up</Link>
+                    {
+                        (userLogin.userLogin === true || adminLogin.adminLogin === true) ? 
+                        ((userLogin.userLogin === true) ?  (<Link to='/signup'>User LoggedIn</Link>) : 
+                        (<Link to='/signup'>AdminLogged In</Link>)) : (<Link to='/signup'>WTF</Link>)
+                    }
+                    
                 </div>
             </div>
         </nav>
